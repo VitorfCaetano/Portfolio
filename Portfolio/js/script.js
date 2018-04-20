02538e4aa54921fe32c3509bcb4a55a6fb2fa09e
Vitor Caetano <viitorcaetano@gmail.com>
2018-04-19 22:22:32 -0300

    Update main.js file using  modular JS

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,55 +1,56 @@
+/*jshint esversion: 6 */
 
-//Variables
-var scrollBtn = document.getElementById('scroll-top');
-var nav = document.getElementById('nav');
-var fade = false;
-var lastScrollTop = 0;
+(() => {
+    //Variables
+    var lastScrollTop = 0;
 
-// Event Listeners
-scrollBtn.addEventListener("click", scrollBackToTop);
-window.addEventListener('scroll', checkScrollToBottom);
+    //Cache DOM
+    const $scrollButton = $('#scroll-top');
+    const $nav = $('#nav');
+    const $navLink = $("#nav ul li a[href^='#']");
 
+    //Bind Events
+    window.addEventListener('scroll', checkScrollToBottom);
+    $scrollButton.on('click', goBackToTop);
+    $navLink.on('click', smoothScroll);
 
-
-//Function to check if the user has scrolled the page
-function checkScrollToBottom(){
-    var st = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-    if (st > 64) {
-        scrollBtn.style.display = "block";
-        nav.classList.add("fade-out");
-
-    } else {
-        scrollBtn.style.display = "none";
+    //Function to make the page go to the top again
+    function goBackToTop() {
+        $('html, body').animate({
+                scrollTop: 0
+            }, 500, function(){}
+        );
     }
 
-    //Check if user is scrolling to top
-    if(st < lastScrollTop){
-        nav.classList.remove("fade-out");
+    // Smooth scrolling function for options in the menu
+    function smoothScroll(e) {
+        // prevent default anchor click behavior
+        e.preventDefault();
+    
+        // store hash
+        var hash = this.hash;
+    
+        // animate
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        });
     }
-    lastScrollTop = st;
-}
 
-
-//Function to make the page go to the top again
-function scrollBackToTop() {
-    $('html, body').animate({
-        scrollTop: 0
-      }, 500, function(){
- 
-      });
-}
-
-// Smooth scrolling function for options in the menu
-$("#nav ul li a[href^='#']").on('click', function(e) {
-
-    // prevent default anchor click behavior
-    e.preventDefault();
- 
-    // store hash
-    var hash = this.hash;
- 
-    // animate
-    $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      });
- });
+    //Function to check if the user has scrolled the page
+    function checkScrollToBottom(){
+        var st = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+        if (st > 64) {
+            $scrollButton.show();
+            $nav.addClass("fade-out");
+    
+        } else {
+            $scrollButton.hide();
+        }
+    
+        //Check if user is scrolling to top
+        if(st < lastScrollTop){
+            $nav.removeClass("fade-out");
+        }
+        lastScrollTop = st;
+    }
+})();
